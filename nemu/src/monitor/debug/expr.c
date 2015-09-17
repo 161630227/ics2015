4cf2431d7456b241f9869e1f4b1b74064e5b6318
tracer-ics2015 <tracer@njuics.org>
2015-09-17 13:21:43 -0400

    >  compile NEMU 141160088 jeremy43 Linux jeremy43 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  13:21:43 up  9:51,  2 users,  load average: 0.00, 0.01, 0.05 27a7462b63b99db4a74542715698fa2c676114e2

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -5,14 +5,14 @@
  */
 #include <sys/types.h>
 #include <regex.h>
-#include<string.h>
-enum {
-	NOTYPE = 256, EQ,W
 
-	/* TODO: Add more token types */
+enum {
+	NOTYPE = 256, EQ
+
+		/* TODO: Add more token types */
 
 };
-int eval(int p,int q);
+
 static struct rule {
 	char *regex;
 	int token_type;
@@ -23,15 +23,8 @@ static struct rule {
 	 */
 
 	{" +",	NOTYPE},				// spaces
-	{"\\+", '+'},
-        {"-", '-'},
-        {"*", '*'},
-        {"/", '/'},					// plus
-	{"==", EQ},
-        {"(",'('},
-        {")",')'},
-        {"[0~9]+",W} 
-						// equal
+	{"\\+", '+'},					// plus
+	{"==", EQ}						// equal
 };
 
 #define NR_REGEX (sizeof(rules) / sizeof(rules[0]) )
@@ -69,7 +62,7 @@ static bool make_token(char *e) {
 	regmatch_t pmatch;
 
 	nr_token = 0;
-	printf("make_tken");
+
 	while(e[position] != '\0') {
 		/* Try all rules one by one. */
 		for(i = 0; i < NR_REGEX; i ++) {
@@ -79,41 +72,16 @@ static bool make_token(char *e) {
 
 				Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s", i, rules[i].regex, position, substr_len, substr_len, substr_start);
 				position += substr_len;
-				printf("true");
+
 				/* TODO: Now a new token is recognized with rules[i]. Add codes
 				 * to record the token in the array ``tokens''. For certain 
 				 * types of tokens, some extra actions should be performed.
 				 */
 
 				switch(rules[i].token_type) {
-					case '+':
-						tokens[nr_token++].type='+';
-						break;
-					case '-':
-						tokens[nr_token++].type='-';
-						break;
-					case '*':
-						tokens[nr_token++].type='*';
-						break;
-					case '/':
-						tokens[nr_token++].type='/';
-						break;
-					case W:
-						tokens[nr_token].type=W;
-						int kk=0;
-						for ( kk=0;kk<substr_len;++kk)
-							tokens[nr_token].str[kk]=e[nr_token+kk];
-						nr_token++;
-						break;
-					case '(':
-						tokens[nr_token++].type='(';
-						break;
-					case ')':
-						tokens[nr_token++].type=')';
-						break;
 					default: panic("please implement me");
 				}
-				printf("%d\n",tokens[nr_token-1].type);
+
 				break;
 			}
 		}
@@ -126,127 +94,18 @@ static bool make_token(char *e) {
 
 	return true; 
 }
-//nr_token--;
 
-/*
-static bool make_token(char *e)
-{
-	printf("hello");
-return false;
-}
-*/
-
-int find(int p,int q)
-{
-
-      int k=q;
-      int op;
-      bool index=false;//decide whether have found op
-      while(k>p)
-      {
-
-          if((tokens[k].type=='+')||(tokens[k].type=='-'))
-          {
-            op=k;
-            index=true;
-            break;
-          }
-          else if((tokens[k].type==')')&&k>p)
-                {
-                    while((tokens[k].type!='('&&(k>p))) k--;
-                }
-          k--;
-       }
-      if(!index)
-      {
-          k=q;
-          while(k>p)
-          {
-             if(((tokens[k].type=='*')||(tokens[k].type=='/'))&&index)
-             {
-               op=k;
-               index=true;
-               break;
-             }
-             else if((tokens[k].type==')')&&k>p)
-                {
-                    while((tokens[k].type!='('&&(k>p))) k--;
-                }
-            k--;
-          }
-     }
-   if (index) return op;
-   else return p;
-} bool check_parentheses(int p,int q)
-{
-        if(tokens[p].type=='('&&tokens[q].type==')') return true;
-        else return false;
-}
-
-uint32_t expr(char *e,bool *success) {
-	printf("woolala\n");
-	/*
-	if(!success)
-	{
-		bool temp=true;
-		success=&temp;
-		//  printf("123");
-	}
-	*/
-	printf("124");
+uint32_t expr(char *e, bool *success) {
+	printf("123");
+	printf("woolala");
 	if(!make_token(e)) {
-		printf("expr");	
 		*success = false;
 		return 0;
 	}
-	else return eval(0,nr_token-1);
 
-}      
-int eval(int p,int q)
-        {
-           if(p>q)
-           {
-            printf("error");
-            return 0;
-           }
-           else if(p==q)
-           {
-             int k=0;
-             int sum=0;
-             for(k=0;k<=strlen(tokens[p].str)-1;++k)
-             {
-               sum*=10;
-               sum+=tokens[p].str[k]-'0'; 
-             }
-             return sum;
-           }
-           else if(check_parentheses(p,q))
-          
-           return eval(p+1,q-1);
-         
-        else
-          {
-          int op=find(p,q);
-          //if(op!=p)
-        
-          int  val1=eval(p,op-1);
-          int  val2=eval(op+1,q);
-          switch(tokens[op].type)
-          {
-            case'+': return val1+val2;
-            case'-': return val1-val2;
-            case'*': return val1*val2;
-            case'/': return val1/val2;
-            default:{
-                    assert(0);
-                    //return 0;
-                    }
-          }
-          
-       }       
-        
-//	TODO: Insert codes to evaluate the expression. */
-      //	panic("please implement me");
-     
+	/* TODO: Insert codes to evaluate the expression. */
+	//panic("please implement me");
+	return 0;
 }
+
 

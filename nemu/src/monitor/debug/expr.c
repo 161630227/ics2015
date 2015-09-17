d30b15529204d60527035ac1f52eda0def1e0fda
tracer-ics2015 <tracer@njuics.org>
2015-09-17 15:51:09 -0400

    >  compile NEMU 141160088 jeremy43 Linux jeremy43 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  15:51:09 up 12:21,  2 users,  load average: 0.01, 0.02, 0.05 96a9cd7f536f4a625a85908a501e8049a845f5e7

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -198,7 +198,7 @@ uint32_t expr(char *e,bool *success) {
 		
 		*success = false;
 		return 0;	}
-	else {  printf("%d",nr_token2);
+	else {  printf("%d",nr_token);
 		return eval(0,nr_token2);
         }
 }      

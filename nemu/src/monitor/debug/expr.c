28ec8c4df1f0d951ca6f6736ad991adc78a22847
tracer-ics2015 <tracer@njuics.org>
2015-09-17 12:55:07 -0400

    >  compile NEMU 141160088 jeremy43 Linux jeremy43 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  12:55:07 up  9:24,  2 users,  load average: 0.09, 0.04, 0.05 daa4b29a1361e13ce52863672afa2fc87e350add

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -69,7 +69,7 @@ static bool make_token(char *e) {
 	regmatch_t pmatch;
 	
 	nr_token = 0;
-        printf("make_token");
+        printf("make_tken");
 	while(e[position] != '\0') {
 		/* Try all rules one by one. */
 		for(i = 0; i < NR_REGEX; i ++) {

833e5a04cfec9ab557351d553e6c46937257cd22
tracer-ics2015 <tracer@njuics.org>
2015-09-18 05:18:44 -0400

    >  compile NEMU 141160088 jeremy43 Linux jeremy43 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  05:18:44 up 1 day,  1:48,  3 users,  load average: 0.00, 0.01, 0.05 928aaef91b509758d3dd3a7cc3e768ead746397

--- nemu/src/monitor/debug/watchpoint.c
+++ nemu/src/monitor/debug/watchpoint.c
@@ -4,7 +4,7 @@
 #define NR_WP 32
 
 static WP wp_list[NR_WP];
-static WP *head, *free_;
+static WP *head, *free_,*index1;
 
 void init_wp_list() {
 	int i;
@@ -19,5 +19,30 @@ void init_wp_list() {
 }
 
 /* TODO: Implement the functionality of watchpoint */
-
+WP* new_wp()
+{
+      index1=free_;	
+      free_=free_->next;
+      if(free_==NULL) 
+      {
+	   printf("no more free");
+	   return NULL;
+      }
+      return (index1);
+}
+void free_wp(WP *wp)
+{
+    index1=free_;
+    if(index1!=NULL)
+    {
+	    free_=wp;
+	    free_->NO=wp->NO;
+            free_->next=index1;	    
+    }
+   else
+    {
+	    free_=wp;
+	    free_->next=NULL;
+    }	    
+}
 

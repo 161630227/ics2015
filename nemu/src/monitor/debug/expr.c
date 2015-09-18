84542e7dfa83cef699b1ef20da28b3817397b70e
tracer-ics2015 <tracer@njuics.org>
2015-09-18 13:13:19 -0400

    >  compile NEMU 141160088 jeremy43 Linux jeremy43 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  13:13:19 up 1 day,  9:43,  5 users,  load average: 0.13, 0.04, 0.05 9d2dbc758a7e9dc46c718787009109b3c29c532

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -221,6 +221,7 @@ int find(int p,int q)
 		    if(((tokens[k].type==NOT)||(tokens[k].type==EQ))&&(!index))
 		    {
 		      op=k;
+		      printf("here\n");
 		      index=true;
 		      break;
 		    }

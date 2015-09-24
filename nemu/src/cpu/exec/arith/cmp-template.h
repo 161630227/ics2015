bf64082c6784a266712d5b25106e77ec33885a16
tracer-ics2015 <tracer@njuics.org>
2015-09-23 20:38:14 -0400

    >  compile NEMU 141160088 jeremy43 Linux jeremy43 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  20:38:14 up 1 day, 18:25,  4 users,  load average: 0.01, 0.04, 0.05 95c25ccb3f728d4f37473405a5752cdb57f30be

---
+++ nemu/src/cpu/exec/arith/cmp-template.h
@@ -0,0 +1,31 @@
+
+
+#include "cpu/exec/template-start.h"
+
+#define instr cmp
+static void do_execute(){
+	DATA_TYPE_S result = op_dest->val - op_src->val;
+	
+
+	/* TODO: Update EFLAGS. */
+        DATA_TYPE_S index=result;
+        cpu.eflags.CF=result<op_dest->val;
+	cpu.eflags.ZF=(result==0);
+	cpu.eflags.PF=1;
+	while (index)
+       {
+	       if(index&0x1) cpu.eflags.PF=!cpu.eflags.PF;
+	       index&=(index-1);
+        }
+        cpu.eflags.AF=(result^(op_dest->val))>>4;
+        cpu.eflags.SF=result<0;
+        cpu.eflags.OF=(((op_src->val<0)!=(op_dest->val<0))&&((op_dest->val<0)!=(result<0)));
+print_asm_template2();
+}
+make_instr_helper(i2rm)
+make_instr_helper(r2rm)
+make_instr_helper(rm2r)
+#if (DATA_BYTE == 2 || DATA_BYTE == 4)
+make_instr_helper(si2rm)
+#endif
+#include "cpu/exec/template-end.h"

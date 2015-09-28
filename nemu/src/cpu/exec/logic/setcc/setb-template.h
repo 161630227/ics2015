99632ae87e278ecd94efa454a8ec1ebf3bfea38a
tracer-ics2015 <tracer@njuics.org>
2015-09-28 15:59:45 -0400

    >  compile NEMU 141160088 jeremy43 Linux jeremy43 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  15:59:45 up 12:03,  3 users,  load average: 0.00, 0.01, 0.05 188742389ac05efe93983df52d9f5aaa982855b5

---
+++ nemu/src/cpu/exec/logic/setcc/setb-template.h
@@ -0,0 +1,18 @@
+
+#include "cpu/exec/template-start.h"
+
+#define instr setb
+
+static void do_execute () {
+	int  index;
+	if(cpu.eflags.CF==1)
+      	 index=1;
+	else index=0;
+	OPERAND_W(op_dest, index);
+       // else OPERAND_W(op_dest,0);
+	print_asm_template1();
+}
+
+make_instr_helper(rm)
+
+#include "cpu/exec/template-end.h"
---
+++ nemu/src/cpu/exec/logic/setcc/setb.h
@@ -0,0 +1,7 @@
+
+#ifndef __SETB_H__
+#define __SETB_H__
+
+make_helper(setb_rm_b);
+
+#endif

ca0d733903abf8f75a8f15ecee83df5f8ac7cf62
tracer-ics2015 <tracer@njuics.org>
2015-10-01 10:37:41 -0400

    >  compile NEMU 141160088 jeremy43 Linux jeremy43 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  10:37:41 up 1 day,  6:40,  4 users,  load average: 0.00, 0.01, 0.05 14c6f5347aecc69fc07a0b11d6c17e829351575

--- nemu/src/cpu/exec/arith/adc-template.h
+++ nemu/src/cpu/exec/arith/adc-template.h
@@ -8,17 +8,17 @@ static void do_execute(){
 	OPERAND_W(op_dest, result);
 
 	/* TODO: Update EFLAGS. */
-	DATA_TYPE index=result;
+	DATA_TYPE_S index=result;
         //cpu.eflags.CF=result<op_dest->val;
 	cpu.eflags.ZF=(result==0);
 	cpu.eflags.PF=1;
+        cpu.eflags.SF=index<0;
 	while (index)
        {
 	       if(index&0x1) cpu.eflags.PF=!cpu.eflags.PF;
 	       index&=(index-1);
         }
         cpu.eflags.AF=(result^(op_dest->val))>>4;
-        cpu.eflags.SF=result<0;
 	if (MSB(op_src->val)!=MSB(op_dest->val)&&(MSB(op_dest->val)!=MSB(result))) cpu.eflags.OF = 1; else cpu.eflags.OF = 0;
 print_asm_template2();  
 }

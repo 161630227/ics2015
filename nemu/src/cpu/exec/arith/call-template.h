c301c51d887fd750e0f9c057d7c4234043871ef5
tracer-ics2015 <tracer@njuics.org>
2015-09-30 23:17:01 -0400

    >  compile NEMU 141160088 jeremy43 Linux jeremy43 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  23:17:01 up 19:19,  4 users,  load average: 0.08, 0.03, 0.05 957535ebe0091a94d30af0e7770753bb7d48885b

--- nemu/src/cpu/exec/arith/call-template.h
+++ nemu/src/cpu/exec/arith/call-template.h
@@ -12,6 +12,7 @@ printf("%x\n",addr);
 	   cpu.esp-=DATA_BYTE;
 	   MEM_W(cpu.esp,cpu.eip+DATA_BYTE);
 	   cpu.eip+=op_src->val;
+printf("%x\n",op_src->val);
 	   print_asm_template1();
            return 1+DATA_BYTE;
 }

111c182d3b85a923159a139071c1d648dc2a7208
tracer-ics2015 <tracer@njuics.org>
2015-09-30 20:22:23 -0400

    >  compile NEMU 141160088 jeremy43 Linux jeremy43 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  20:22:23 up 16:24,  3 users,  load average: 0.00, 0.02, 0.05 4c2224f92edf53ee270ea6a524d8f2a1b8f6c07f

--- nemu/src/cpu/exec/data-mov/pop-template.h
+++ nemu/src/cpu/exec/data-mov/pop-template.h
@@ -7,12 +7,7 @@
 
 //#if DATA_BYTE == 2 || DATA_BYTE == 4
 static void do_execute() {
-	//	cpu.esp=cpu.esp+DATA_BYTE;
-
-	      //	swaddr_write(op_dst,DATA_BYTE,op_src->val);
-	     //	swaddr_write(dest, DATA_BYTE,REG[R_ESP], DATA_BYTE);
-	       if (DATA_BYTE==4) OPERAND_W(op_dest, reg_l(R_ESP));
-	       else OPERAND_W(op_dest,reg_w(R_ESP)); 
+	       op_dest->val=MEM_R(cpu.esp);
 	       cpu.esp=cpu.esp+DATA_BYTE;
 		print_asm_template1();
 }

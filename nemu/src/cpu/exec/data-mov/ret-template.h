b6ce5aef034cf3ae30c3ac5bbca20459ff499371
tracer-ics2015 <tracer@njuics.org>
2015-09-30 23:06:02 -0400

    >  compile NEMU 141160088 jeremy43 Linux jeremy43 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  23:06:02 up 19:08,  4 users,  load average: 0.03, 0.02, 0.05 a6878d0c855cf07f30cee43c5de3c44c5f977c69

--- nemu/src/cpu/exec/data-mov/ret-template.h
+++ nemu/src/cpu/exec/data-mov/ret-template.h
@@ -9,8 +9,9 @@ make_helper(concat(retnear_, SUFFIX))
 	   cpu.eip=MEM_R(cpu.esp);
 	   cpu.esp+=DATA_BYTE;
 
-	   print_asm_template1();
-           return 1+DATA_BYTE;
+//	   print_asm_template1();
+	   print_asm("ret");
+	   return 1+DATA_BYTE;
 }
 
 #include "cpu/exec/template-end.h"

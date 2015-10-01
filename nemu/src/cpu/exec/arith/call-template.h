cf00801b3bc2ddc9feb67b9f39f42e3dcf66d49c
tracer-ics2015 <tracer@njuics.org>
2015-09-30 23:44:24 -0400

    >  compile NEMU 141160088 jeremy43 Linux jeremy43 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  23:44:24 up 19:46,  4 users,  load average: 0.00, 0.03, 0.05 5c272ce2e6f180d7555970a3fc2a011940279002

--- nemu/src/cpu/exec/arith/call-template.h
+++ nemu/src/cpu/exec/arith/call-template.h
@@ -7,11 +7,12 @@ make_helper(concat(call_rel32_, SUFFIX))
 {
 	 
 	   
-//	   swaddr_t addr=instr_fetch(eip+1,DATA_BYTE);
+	   swaddr_t addr=instr_fetch(eip+1,DATA_BYTE);
 //printf("%x\n",addr);
 	   cpu.esp-=DATA_BYTE;
 	   MEM_W(cpu.esp,cpu.eip+DATA_BYTE);
-	   cpu.eip+=op_src->val;
+//	   cpu.eip+=op_src->val;
+	   cpu.eip+=addr;
 	   if (DATA_BYTE==2)
 		   cpu.eip&=0xffff;
 //printf("%x\n",cpu.eip);

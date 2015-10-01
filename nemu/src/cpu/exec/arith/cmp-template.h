cf0f70fbb860085079a5fe586e3c52154f104cf8
tracer-ics2015 <tracer@njuics.org>
2015-10-01 04:20:49 -0400

    >  compile NEMU 141160088 jeremy43 Linux jeremy43 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  04:20:49 up 1 day, 23 min,  4 users,  load average: 0.00, 0.01, 0.05 f1d0407bf7bb452d30b52211a5956cd3cfcfa3e6

--- nemu/src/cpu/exec/arith/cmp-template.h
+++ nemu/src/cpu/exec/arith/cmp-template.h
@@ -18,7 +18,7 @@ static void do_execute(){
 	       index&=(index-1);
         }
         cpu.eflags.AF=(result^(op_dest->val))>>4;
-        cpu.eflags.SF=result<0;
+        cpu.eflags.SF=index<0;
 //        cpu.eflags.OF=(((op_src->val<0)!=(op_dest->val<0))&&((op_dest->val<0)!=(result<0)));
 	cpu.eflags.OF = (MSB(op_src->val)!=MSB(op_dest->val)&&(MSB(op_dest->val!=MSB(result))));
 print_asm_template2();

9757e677e3c88d59f3b7f54d81d9d91bacc32086
tracer-ics2015 <tracer@njuics.org>
2015-10-01 02:56:36 -0400

    >  compile NEMU 141160088 jeremy43 Linux jeremy43 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  02:56:36 up 22:58,  4 users,  load average: 0.00, 0.01, 0.05 a624d90a79acca3fb98240c6c7e3ea247c8a252c

--- nemu/src/cpu/exec/arith/cmp-template.h
+++ nemu/src/cpu/exec/arith/cmp-template.h
@@ -25,6 +25,7 @@ print_asm_template2();
 }
 make_instr_helper(i2rm)
 make_instr_helper(r2rm)
+make_instr_helper(i2a)
 make_instr_helper(rm2r)
 #if (DATA_BYTE == 2 || DATA_BYTE == 4)
 make_instr_helper(si2rm)

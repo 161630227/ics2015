f9ff30d452efeb72998b6c40ae600ea170bed027
tracer-ics2015 <tracer@njuics.org>
2015-10-02 02:19:44 -0400

    >  compile NEMU 141160088 jeremy43 Linux jeremy43 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  02:19:44 up 1 day, 22:22,  3 users,  load average: 0.00, 0.01, 0.05 17677d37c03fe7cae4e1df256bab0fa9530fa77

--- nemu/src/cpu/exec/data-mov/movsx-template.h
+++ nemu/src/cpu/exec/data-mov/movsx-template.h
@@ -3,6 +3,7 @@
 //#include "cpu/decode/modrm.h"
 #define instr movsx
 static void do_execute(){
+	printf("%x\n",op_dest->val);
 	DATA_TYPE_S index=op_src->val;
 	index=(index<<(DATA_BYTE*8-8)>>(DATA_BYTE*8-8));
 	OPERAND_W(op_dest,index);

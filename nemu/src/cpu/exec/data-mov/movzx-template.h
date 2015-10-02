5e4b04116cb61e1c71f498956ef3a76be01a4f57
tracer-ics2015 <tracer@njuics.org>
2015-10-02 10:11:14 -0400

    >  compile NEMU 141160088 jeremy43 Linux jeremy43 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  10:11:14 up 2 days,  6:13,  5 users,  load average: 0.02, 0.03, 0.05 54b5bcacc25d547d639361f09f11ac49414eee36

--- nemu/src/cpu/exec/data-mov/movzx-template.h
+++ nemu/src/cpu/exec/data-mov/movzx-template.h
@@ -3,14 +3,14 @@
 //#include "cpu/decode/modrm.h"
 #define instr movzx
 static void do_execute(){
-        printf("****");
+ //       printf("****");
 	DATA_TYPE index=op_src->val;
      //  if(DATA_BYTE==2)index&=0x00ff;
       // if(DATA_BYTE==4)index&=0x0000ffff;       
 	OPERAND_W(op_dest,index);
 }
 make_helper(concat(movzx_rmb2r_,SUFFIX)){
-       printf("***\n");
+     //  printf("***\n");
 	return idex(eip,decode_rm2r_b,concat(do_movzx_,SUFFIX));
 }
 #include "cpu/exec/template-end.h"

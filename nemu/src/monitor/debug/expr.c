9de90717a0e7a0675bb7ab484346cfa71b84e3db
tracer-ics2015 <tracer@njuics.org>
2015-10-02 12:22:50 -0400

    >  compile NEMU 141160088 jeremy43 Linux jeremy43 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  12:22:50 up 2 days,  8:25,  5 users,  load average: 0.00, 0.01, 0.05 5eef97ee625e410ccd8eb8cd4ae3e68167485957

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -81,10 +81,10 @@ static int make_token(char *e) {
 		/* Try all rules one by one. */
 		for(i = 0; i < NR_REGEX; i ++) {
 			if(regexec(&re[i], e + position, 1, &pmatch, 0) == 0 && pmatch.rm_so == 0) {
-				char *substr_start = e + position;
+			//	char *substr_start = e + position;
 				int substr_len = pmatch.rm_eo;
                                
-				Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s", i, rules[i].regex, position, substr_len, substr_len, substr_start);
+				//Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s", i, rules[i].regex, position, substr_len, substr_len, substr_start);
 				position += substr_len;
 				
 				/* TODO: Now a new token is recognized with rules[i]. Add codes
@@ -366,7 +366,7 @@ int eval(int p,int q)
 		   return swaddr_read(sum7,1);
 	           */
 		  sum7=eval(p+1,q);
-	          printf("*  %x\n",sum7);
+	          //printf("*  %x\n",sum7);
 		  return (swaddr_read(sum7,4));
 
 	 } 

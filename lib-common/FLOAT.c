01420dc0ea60deda401c5922760e337a432b8456
tracer-ics2015 <tracer@njuics.org>
2015-10-05 02:05:06 -0400

    >  compile NEMU 141160088 jeremy43 Linux jeremy43 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  02:05:06 up 18:03,  3 users,  load average: 0.28, 0.08, 0.07 63389f0121b7b92441b99ecf522347c6bddf4b50

--- lib-common/FLOAT.c
+++ lib-common/FLOAT.c
@@ -1,22 +1,78 @@
 #include "FLOAT.h"
 
 FLOAT F_mul_F(FLOAT a, FLOAT b) {
-	nemu_assert(0);
-	return 0;
+        int index=0;
+	if(a<0)
+	{
+		a=-a;
+		index++;
+	}
+	if(b<0)
+	{
+	       b=-b;
+	       index++;
+	}
+	unsigned int a1=(a>>16);
+	unsigned int  b0=b&0xffff;
+	unsigned int  a0=a&0xffff;
+	unsigned int b1=(b>>16);
+	unsigned int  c0=(a0*b0)/0xffff;
+	unsigned int c1=a1*b0+a0*b1+c0;
+	unsigned int c2=(a1*b1)<<16;
+	c2+=c1;
+	if(index%2==0) return c2;
+	  else return -c2;
 }
 
 FLOAT F_div_F(FLOAT a, FLOAT b) {
-	nemu_assert(0);
-	return 0;
+  unsigned int a,b,i,ans,nb;
+   nb=31; 
+  unsigned int  a0=a&0xffff;
+   unsigned int  a1=(a>>16);
+   unsigned int  a11=(a0<<16);
+   ans=0;
+   for (i=1;i<=32;++i)
+   {
+	   if(a1>=b)							              {
+									                 ans+=a1/b;								     a1=a1%b;
+ 	   }	
+	   a1<<=1;							             a1+=(a11>>nb);							        if(a11>>nb) a11-=(1<<nb);ans=(ans<<1);					   nb--;
+   }	
+   return ans;
 }
 
 FLOAT f2F(float a) {
-	nemu_assert(0);
-	return 0;
+	int b=*(int *)&a;
+	unsigned int  b1=b;
+	int mid=b>>23;
+	
+	if(b<0)mid=mid&0xff;
+	unsigned int back;
+	if(b<0) back=b1-(mid<<23)-(1<<31);
+	  else  back=b1-(mid<<23);
+	back+=(1<<23);
+	if(mid>=127)
+	{
+		mid-=127;
+		mid+=16;
+                if(mid>=23) back<<=(mid-23);
+		else
+		{	back=back>>(23-mid);
+		}
+	}
+	else
+	{
+		int mid1=mid-127;
+                mid1+=16;
+		back>>=(23-mid1);
+	}
+        int back1=back;
+	if(b<0) back1=-back1;
+	return back1;
 }
 
 FLOAT Fabs(FLOAT a) {
-	nemu_assert(0);
+	return a >=0 ? a : -a;
 	return 0;
 }
 

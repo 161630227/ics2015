7c82f2a795b9456191f05c208488fdd98fbda229
tracer-ics2015 <tracer@njuics.org>
2015-10-09 19:32:18 -0400

    >  compile NEMU 141160088 jeremy43 Linux jeremy43 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  19:32:18 up 14:53,  5 users,  load average: 0.00, 0.02, 0.08 b37f46a39ef51e7d8187f5fc3b4d032d0d24e8d

--- nemu/src/cpu/exec/arith/inc-template.h
+++ nemu/src/cpu/exec/arith/inc-template.h
@@ -5,9 +5,20 @@
 static void do_execute () {
 	DATA_TYPE result = op_src->val + 1;
 	OPERAND_W(op_src, result);
-
-	/* TODO: Update EFLAGS. */
-	panic("please implement me");
+       
+	DATA_TYPE_S index=result;
+        //cpu.eflags.CF=result<op_dest->val;
+	cpu.eflags.ZF=(result==0);
+	cpu.eflags.PF=1;
+        cpu.eflags.SF=index<0;
+	while (index)
+       {
+	       if(index&0x1) cpu.eflags.PF=!cpu.eflags.PF;
+	       index&=(index-1);
+        }
+        cpu.eflags.AF=(result^(op_src->val))>>4;
+        if(MSB(op_src->val) == 0 && MSB(result) != MSB(op_src->val))
+	   cpu.eflags.OF = 1; else cpu.eflags.OF = 0;
 
 	print_asm_template1();
 }

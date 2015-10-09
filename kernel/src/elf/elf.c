09362a62208211200846d9afae65ddf2d54a05c8
tracer-ics2015 <tracer@njuics.org>
2015-10-09 18:46:05 -0400

    >  compile NEMU 141160088 jeremy43 Linux jeremy43 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  18:46:05 up 14:07,  5 users,  load average: 0.22, 0.06, 0.06 88c87f858f5d251217ee0114ddf3cd3963f95093

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -8,9 +8,10 @@
 #ifdef HAS_DEVICE
 void ide_read(uint8_t *, uint32_t, uint32_t);
 #else
+
 void ramdisk_read(uint8_t *, uint32_t, uint32_t);
 #endif
-
+#define ramdisk_write(uint8_t*,uint32_t,uint32_t);
 #define STACK_SIZE (1 << 20)
 
 void create_video_mapping();
@@ -31,21 +32,26 @@ uint32_t loader() {
 	elf = (void*)buf;
 
 	/* TODO: fix the magic number with the correct one */
-	const uint32_t elf_magic = 0xBadC0de;
+	const uint32_t elf_magic = 0x464c457f;
 	uint32_t *p_magic = (void *)buf;
 	nemu_assert(*p_magic == elf_magic);
-
+       int i;
 	/* Load each program segment */
-	panic("please implement me");
-	for(; true; ) {
+//	panic("please implement me");
+	for(i=0;i<elf->e_phnum;++i) {
 		/* Scan the program header table, load each segment into memory */
+	ph = (void*)(buf + elf->e_ehsize + i * elf->e_phentsize);
 		if(ph->p_type == PT_LOAD) {
 
 			/* TODO: read the content of the segment from the ELF file 
 			 * to the memory region [VirtAddr, VirtAddr + FileSiz)
 			 */
+			int j;
+			ramdisk_write(buf + ph->p_offset, ph->p_vaddr, ph->p_filesz);
 			 
-			 
+	                 uin8_t zero=0;
+	                for(j=ph->p_filesz;j<ph->p_memsz;++j)
+				ramdisk_write(&zero, ph->p_vaddr + j, 1);
 			/* TODO: zero the memory region 
 			 * [VirtAddr + FileSiz, VirtAddr + MemSiz)
 			 */

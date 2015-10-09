6b76db6daf715ca4599f35a068cc444610b7ae30
tracer-ics2015 <tracer@njuics.org>
2015-10-09 19:10:40 -0400

    >  compile kernel 141160088 jeremy43 Linux jeremy43 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  19:10:40 up 14:32,  5 users,  load average: 0.12, 0.05, 0.05 115da23f8b478f1c1a704fcbba806d4d3037761

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -21,12 +21,12 @@ uint32_t loader() {
 	Elf32_Ehdr *elf;
 	Elf32_Phdr *ph = NULL;
 
-	uint8_t buf[4096];
+	uint8_t buf[0x100000];
 
 #ifdef HAS_DEVICE
-	ide_read(buf, ELF_OFFSET_IN_DISK, 4096);
+	ide_read(buf, ELF_OFFSET_IN_DISK, 0x100000);
 #else
-	ramdisk_read(buf, ELF_OFFSET_IN_DISK, 4096);
+	ramdisk_read(buf, ELF_OFFSET_IN_DISK, 0x100000);
 #endif
 
 	elf = (void*)buf;

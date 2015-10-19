6939dec751140966d316400ca5bf525f35fb61a0
tracer-ics2015 <tracer@njuics.org>
2015-10-19 03:52:12 -0400

    >  compile NEMU 141160088 jeremy43 Linux jeremy43 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  03:52:12 up  1:08,  4 users,  load average: 0.01, 0.02, 0.05 a6708b2f4e10e098f99541b392d2d2d1b7d626da

--- nemu/src/memory/cache-template.h
+++ nemu/src/memory/cache-template.h
@@ -79,7 +79,7 @@ bool concat(cache_write_,LEVEL)(uint8_t *data,uint32_t addr,uint32_t size,bool n
 	uint32_t set_index=(addr & GET_SET_INDEX)>>BLOCK_BYTE;
 	int i;
 	uint32_t head_addr=addr & GET_HEAD_ADDR;
-	printf("data= %x\n",*data);
+	printf("datahere %x\n",*data);
 	uint32_t block_offset=addr & GET_BLOCK_OFFSET;
 	if(not_read)
 	{

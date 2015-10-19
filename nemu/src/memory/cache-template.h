31b1e2c65b0442875c294b61a5c056fd5b69d00a
tracer-ics2015 <tracer@njuics.org>
2015-10-19 04:44:13 -0400

    >  compile NEMU 141160088 jeremy43 Linux jeremy43 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  04:44:13 up  2:00,  5 users,  load average: 0.00, 0.05, 0.08 8928aea2dae1fb38aa54eee87aab8377efdab86d

--- nemu/src/memory/cache-template.h
+++ nemu/src/memory/cache-template.h
@@ -118,11 +118,11 @@ bool concat(cache_write_,LEVEL)(uint8_t *data,uint32_t addr,uint32_t size,bool n
 			cache_LEVEL[set_index].cache_line[i].valid=true;
 			for (j=0;j<size;j++,data++)
 			{
-				printf("j=%x\n",*data);
+			//	printf("j=%x\n",*data);
 				cache_LEVEL[set_index].cache_line[i].block[block_offset+j]=*data;
                        // printf("data%x\n",*data);
 			}
-			printf("cache%x\n",cache_LEVEL[set_index].cache_line[i].block[block_offset]);
+			//printf("cache%x\n",cache_LEVEL[set_index].cache_line[i].block[block_offset]);
 			if(l2)
 				cache_LEVEL[set_index].cache_line[i].dirty=true;
                   return true;                   

4bdde9612597f11b8c6baeb52c038f7cdabc88d0
tracer-ics2015 <tracer@njuics.org>
2015-10-19 06:03:35 -0400

    >  compile NEMU 141160088 jeremy43 Linux jeremy43 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  06:03:35 up  3:19,  6 users,  load average: 0.02, 0.02, 0.05 7b16ee2a11b1187d50cf7d67113f923187b3297a

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -17,7 +17,8 @@ uint32_t hwaddr_read(hwaddr_t addr, size_t len) {
 	assert(len==1||len==2||len==4);
 	bool data_hit=false;
 	uint32_t result;
-    	printf("dram %x ",dram(0x00123000,1));
+	printf("addr=%x\n",addr);
+//    	printf("dram %x ",dram(0x00123000,1));
 /*	result=dram_read(0x12345678,1);
         cache_write_l1(&result,0x12345678,1,0,0);
 	printf("cache  %x\n",cache_read_l1(&data_hit,0x12345678,1));*/

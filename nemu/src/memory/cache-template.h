9c5e4b9241e061d5e0c9d281c297d86646f66214
tracer-ics2015 <tracer@njuics.org>
2015-10-19 04:32:28 -0400

    >  compile NEMU 141160088 jeremy43 Linux jeremy43 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  04:32:28 up  1:48,  5 users,  load average: 0.55, 0.18, 0.10 ab6ccce0c9c0c56479320ade49b6a38f2e3296a3

--- nemu/src/memory/cache-template.h
+++ nemu/src/memory/cache-template.h
@@ -57,7 +57,7 @@ uint32_t concat(cache_read_,LEVEL)(bool *hit,uint32_t addr, size_t len)
 				*hit=true;
 				int j;
 				uint32_t index2=cache_LEVEL[set_index].cache_line[i].block[block_offset+len-1];
-				
+			        printf("%%\n");	
 				for (j=len-2;j>=0;++j)
                                 {
 					index2<<=8;

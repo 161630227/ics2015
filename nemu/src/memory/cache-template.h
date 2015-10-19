78833676e194cd91a0dd01a0484b972d0dc5c5c3
tracer-ics2015 <tracer@njuics.org>
2015-10-19 19:24:05 -0400

    >  compile NEMU 141160088 jeremy43 Linux jeremy43 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  19:24:05 up 16:40,  6 users,  load average: 0.00, 0.01, 0.05 311820a49eea65eb30abaeb78e836b39da827b56

--- nemu/src/memory/cache-template.h
+++ nemu/src/memory/cache-template.h
@@ -147,20 +147,7 @@ bool concat(cache_write_,LEVEL)(uint32_t* data,uint32_t byte,uint32_t addr,uint3
 	                   	int data1=data_index&(~0u >> ((4 - size) << 3));
 				memcpy(cache_LEVEL[set_index].cache_line[i].block+block_offset,&data1,size);
 			}
-			if(addr==0x10003e)
-			{
-			//	printf("data=%x\n",*data);
-			//	printf("145行%x\n",cache_LEVEL[set_index].cache_line[i].block[block_offset]);
-			  //      printf("146行%x\n",cache_LEVEL[set_index].cache_line[i].block[block_offset+1]);
-			     //   bool data_hit=false;
-			//	int rr=(addr+64-block_offset+1)&GET_BLOCK_OFFSET;
-			       // int rr=(addr+64-block_offset)&GET_BLOCK_OFFSET;
-	                 //	printf("%%126\n");
-			//	printf("index=%x\n",rr);	
-			   //    	printf("cache158  %x\n",cache_read_l1(&data_hit,0x10003e,4));
-			  //      printf("147行%x\n",cache_LEVEL[40].cache_line[i].block[0]);
-		            //    printf("148行%x\n",cache_LEVEL[40].cache_line[i].block[1]);
-			}
+			
 			if(l2)
 				cache_LEVEL[set_index].cache_line[i].dirty=true;
                   

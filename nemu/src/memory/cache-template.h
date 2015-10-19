ae1b21ba18d2d2f90220dd9ac1e0e2b9a7b7ca01
tracer-ics2015 <tracer@njuics.org>
2015-10-19 12:05:01 -0400

    >  compile NEMU 141160088 jeremy43 Linux jeremy43 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  12:05:01 up  9:21,  6 users,  load average: 0.01, 0.02, 0.05 717a61e0dbb031bcfa6e3cd134956b7d782ca5bf

--- nemu/src/memory/cache-template.h
+++ nemu/src/memory/cache-template.h
@@ -25,7 +25,7 @@ uint32_t concat(cache_read_,LEVEL)(bool *hit,uint32_t addr, size_t len)
 				bool hit2=false;
 //				uint32_t result=dram_read(addr+count,len-count)&(~0u >> ((4 - len) << 3));
 				uint32_t result=concat(cache_read_,LEVEL)(&hit2,addr+count,len-count);
-				printf("result1=%x",result);
+				printf("result1=%x\n",result);
 				int j;
 				uint32_t index;
 				index=cache_LEVEL[set_index].cache_line[i].block[63];
@@ -38,6 +38,7 @@ uint32_t concat(cache_read_,LEVEL)(bool *hit,uint32_t addr, size_t len)
 				{
 					*hit=true;
 		//		        printf("dram=   cache=    %x   %x\n",dram(addr,len),(index+(result<<(8*count))));	
+					return(index+(result<<(8*count)));
 				}
 				else
 

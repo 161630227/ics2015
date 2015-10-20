19da75e2f1b60985a2dc6eb190df85c3c67d759d
tracer-ics2015 <tracer@njuics.org>
2015-10-19 23:26:23 -0400

    >  compile NEMU 141160088 jeremy43 Linux jeremy43 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  23:26:23 up 20:42,  6 users,  load average: 0.04, 0.04, 0.05 ed151aa0ad03506ac4e0bae690e8c322bcfa1221

--- nemu/src/memory/cache-template.h
+++ nemu/src/memory/cache-template.h
@@ -80,7 +80,8 @@ uint32_t concat(cache_read_,LEVEL)(bool *hit,uint32_t addr, size_t len)
 bool concat(cache_write_,LEVEL)(uint32_t* data,uint32_t byte,uint32_t addr,uint32_t size,bool not_read,bool l2)//data represent addr
 {
 //    if(size==0)	printf("80行 addr=%x\n",addr);
-	printf("118data1=%x\n",*data);
+      if((*data)!=0)printf("118data1=%x\n",*data);
+ 
 	uint32_t set_index=(addr & GET_SET_INDEX)>>BLOCK_BYTE;
 	int i;
 	uint32_t head_addr=addr & GET_HEAD_ADDR;

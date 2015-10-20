53466a056cae21b301e8c85bc411ddcc4dbda5eb
tracer-ics2015 <tracer@njuics.org>
2015-10-19 22:45:09 -0400

    >  compile NEMU 141160088 jeremy43 Linux jeremy43 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  22:45:09 up 20:01,  6 users,  load average: 0.06, 0.03, 0.05 1306f40c2a7b7ca6bb9a578ab5bd2582204d18dc

--- nemu/src/memory/cache-template.h
+++ nemu/src/memory/cache-template.h
@@ -77,7 +77,7 @@ uint32_t concat(cache_read_,LEVEL)(bool *hit,uint32_t addr, size_t len)
 
 bool concat(cache_write_,LEVEL)(uint32_t* data,uint32_t byte,uint32_t addr,uint32_t size,bool not_read,bool l2)//data represent addr
 {
-	if (addr==0x7f5ffa0) printf("^^");
+	printf("80行 size=%x\n",size);
 	uint32_t set_index=(addr & GET_SET_INDEX)>>BLOCK_BYTE;
 	int i;
 	uint32_t head_addr=addr & GET_HEAD_ADDR;
@@ -197,7 +197,7 @@ bool concat(cache_write_,LEVEL)(uint32_t* data,uint32_t byte,uint32_t addr,uint3
 
 	uint32_t data_index=(*data)>>(8*byte);
 	int data1=data_index&(~0u >> ((4 - size) << 3));
-        printf("size=  %x data1=%x\n",size,data1);
+        printf("200行size=  %x data1=%x\n",size,data1);
 	memcpy(cache_LEVEL[set_index].cache_line[i].block+block_offset,&data1,size);
 //	printf("data1=%x\n",data1);
 	return true;

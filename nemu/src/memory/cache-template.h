3407ccbad23f0c5f4789ed23483c20077e1f821d
tracer-ics2015 <tracer@njuics.org>
2015-10-19 22:46:03 -0400

    >  compile NEMU 141160088 jeremy43 Linux jeremy43 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  22:46:03 up 20:02,  6 users,  load average: 0.03, 0.02, 0.05 4443d29bfae2263410866ecc1ff0e96c8a20b0c

--- nemu/src/memory/cache-template.h
+++ nemu/src/memory/cache-template.h
@@ -197,7 +197,7 @@ bool concat(cache_write_,LEVEL)(uint32_t* data,uint32_t byte,uint32_t addr,uint3
 
 	uint32_t data_index=(*data)>>(8*byte);
 	int data1=data_index&(~0u >> ((4 - size) << 3));
-        printf("200行size=  %x data1=%x\n",size,data1);
+//        printf("200行size=  %x data1=%x\n",size,data1);
 	memcpy(cache_LEVEL[set_index].cache_line[i].block+block_offset,&data1,size);
 //	printf("data1=%x\n",data1);
 	return true;

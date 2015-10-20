b4971c58e2743ce5c78ab463f12ce4a76ff11875
tracer-ics2015 <tracer@njuics.org>
2015-10-20 12:48:55 -0400

    >  compile NEMU 141160088 jeremy43 Linux jeremy43 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  12:48:55 up 1 day, 10:04,  6 users,  load average: 0.00, 0.01, 0.05 f60238a6bad557bbe760011f4a0133e80907cf6

--- nemu/src/cpu/exec/data-mov/mov-template.h
+++ nemu/src/cpu/exec/data-mov/mov-template.h
@@ -12,7 +12,6 @@ make_instr_helper(i2r)
 make_instr_helper(i2rm)
 make_instr_helper(r2rm)
 make_instr_helper(rm2r)
-
 make_helper(concat(mov_a2moffs_, SUFFIX)) {
 	swaddr_t addr = instr_fetch(eip + 1, 4);
 	MEM_W(addr, REG(R_EAX));
@@ -28,5 +27,17 @@ make_helper(concat(mov_moffs2a_, SUFFIX)) {
 	print_asm("mov" str(SUFFIX) " 0x%x,%%%s", addr, REG_NAME(R_EAX));
 	return 5;
 }
+make_helper(concat(mov_rm2cr_,SUFFIX)){
+	uint32_t r=instr_fetch(eip+1,1);
+	cpu.cr0.val=REG(r&0x7);
+	print_asm("mov" str(SUFFIX) " %%%s,%%%s", REG_NAME(r & 0x7), "cr0");
+        return 2;
+}
 
+make_helper(concat(mov_cr2rm_,SUFFIX)){
+	uint32_t r=instr_fetch(eip+1,1);
+	REG(r&0x7)= cpu.cr0.val;
+	print_asm("mov" str(SUFFIX) " %%%s,%%%s","cr0", REG_NAME(r & 0x7));
+        return 2;
+}
 #include "cpu/exec/template-end.h"
--- nemu/src/cpu/exec/data-mov/mov.h
+++ nemu/src/cpu/exec/data-mov/mov.h
@@ -7,7 +7,8 @@ make_helper(mov_r2rm_b);
 make_helper(mov_rm2r_b);
 make_helper(mov_a2moffs_b);
 make_helper(mov_moffs2a_b);
-
+make_helper(mov_rm2cr_l);
+make_helper(mov_cr2rm_l);
 make_helper(mov_i2r_v);
 make_helper(mov_i2rm_v);
 make_helper(mov_r2rm_v);

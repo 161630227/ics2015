28abdf20e84494dfbfaed10f4caedc57d52ba460
tracer-ics2015 <tracer@njuics.org>
2015-10-21 03:08:20 -0400

    >  compile NEMU 141160088 jeremy43 Linux jeremy43 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  03:08:20 up 2 days, 24 min,  6 users,  load average: 0.08, 0.03, 0.05 9cf9f171541e3ca5c61b928fab2c40a1887c4221

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -115,15 +115,26 @@ uint32_t swaddr_read(swaddr_t addr, size_t len,uint8_t sreg) {
 //#ifdef DEBUG
 	assert(len == 1 || len == 2 || len == 4);
 //#endif
-	lnaddr_t lnaddr= seg_translate(addr, len, sreg);      
-	return lnaddr_read(lnaddr, len);
+	if(cpu.cr0.protect_enable)
+	{
+		lnaddr_t lnaddr= seg_translate(addr, len, sreg);      
+		return lnaddr_read(lnaddr, len);
+
+	}
+	else return dram_read(addr,len);
 }
+
 
 void swaddr_write(swaddr_t addr, size_t len, uint32_t data,uint8_t sreg) {
 //#ifdef DEBUG
 	assert(len == 1 || len == 2 || len == 4);
 //#endif
-	 lnaddr_t lnaddr = seg_translate(addr, len, sreg);
-	lnaddr_write(lnaddr, len, data);
+       if(cpu.cr0.protect_enable)
+       {
+	       lnaddr_t lnaddr = seg_translate(addr, len, sreg);
+
+        	lnaddr_write(lnaddr, len, data);
+     }
+       else dram_write(addr,len,data);
 }
 

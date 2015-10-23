f52c72a45359e828ca2327a733bcb3d4d3e879bd
tracer-ics2015 <tracer@njuics.org>
2015-10-22 22:45:52 -0400

    >  compile NEMU 141160088 jeremy43 Linux jeremy43 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  22:45:52 up  2:10,  5 users,  load average: 0.00, 0.01, 0.05 77ed86486cbaf2a0793f96571e2463749cf5ab97

--- nemu/src/cpu/exec/data-mov/lgdt-template.h
+++ nemu/src/cpu/exec/data-mov/lgdt-template.h
@@ -3,7 +3,9 @@
 #define instr lgdt
 
 make_helper(concat(lgdt_,SUFFIX)) {
+   printf("**\n");
     lnaddr_t addr = instr_fetch(eip + 2, 4);
+    printf("^^");
     cpu.gdtr.limit = lnaddr_read(addr, 2);
     cpu.gdtr.base_addr = lnaddr_read(addr + 2, 4);
     print_asm("lgdt" str(SUFFIX) " 0x%x", addr); 

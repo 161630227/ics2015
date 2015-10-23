95dca81e03af0ae5d8bc1d71356ecdbb1cdb98eb
tracer-ics2015 <tracer@njuics.org>
2015-10-23 05:15:25 -0400

    >  compile NEMU 141160088 jeremy43 Linux jeremy43 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  05:15:25 up  8:40,  6 users,  load average: 0.01, 0.02, 0.05 7500dbd0652458d0622f417441ae72db9812a28

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -111,6 +111,8 @@ lnaddr_t  seg_translate(uint32_t addr,size_t len,uint8_t sreg)
 	for(i = 0; i < 8; ++ i) 
 	tmp[i] = lnaddr_read(cpu.gdtr.base_addr + reg_index * 8 + i, 1);
 	SegDesc *segdesc = (SegDesc*)tmp;
+	uint32_t dd1=(uint32_t)&tmp[0];
+	printf("addr %d\n",dd1);
 	printf("SEGDESC= %x\n",(segdesc->base_23_16 << 16));
 	
 	return (segdesc->base_31_24 << 24) + (segdesc->base_23_16 << 16) + 	segdesc->base_15_0 + addr;

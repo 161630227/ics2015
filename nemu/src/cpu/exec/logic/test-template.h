b99986abcc8ebc809043ce56049445c3c08c3a4c
tracer-ics2015 <tracer@njuics.org>
2015-10-27 14:05:14 -0400

    >  compile NEMU 141160088 jeremy43 Linux jeremy43 3.16.0-4-686-pae #1 SMP Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  14:05:14 up 23:05,  7 users,  load average: 0.00, 0.01, 0.05 e4b2f169b8b56118d693e003dbae4f4b98e20aff

--- nemu/src/cpu/exec/logic/test-template.h
+++ nemu/src/cpu/exec/logic/test-template.h
@@ -5,7 +5,7 @@
 
 static void do_execute () {
 //	printf("ss\n");
-	DATA_TYPE_S index = op_dest->val & op_src->val;
+	DATA_TYPE index = op_dest->val & op_src->val;
 //	OPERAND_W(op_dest, result);
 
 	/* TODO: Update EFLAGS. */
